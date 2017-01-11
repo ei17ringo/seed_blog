ada99b21f23436fedfcc0659c1c6efcf1207421f
ei17ringo <ei17ringo@gmail.com>
2017-01-11 16:05:40 +0800

    DB接続 HTML表示（view) application.php index.php 2017/01/11

--- controllers/blogs_controller.php
+++ controllers/blogs_controller.php
@@ -1,5 +1,5 @@
 <?php
-	echo 'blogsコントローラーが呼ばれました<br />';
+	// echo 'blogsコントローラーが呼ばれました<br />';
 
 	//モデルの呼び出し
 	require('models/blog.php');
@@ -23,14 +23,17 @@
 
 	class BlogsController {
      function index() {
-       echo 'コントローラのindex()が呼び出されました！<br />';
+       // echo 'コントローラのindex()が呼び出されました！<br />';
        // モデルを呼び出す
        $blog = new Blog();
-       $blog->index();
+       $viewOptions = $blog->index();
+       $action = 'index';
+       // var_dump($viewOptions);
+       require('views/layout/application.php');
      }
 
      function add(){
-     	echo 'add()が呼び出されました。<br />';
+     	// echo 'add()が呼び出されました。<br />';
      }
    }
 ?>
--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
-echo 'routes.phpを通りました';
-echo '<br />';
+// echo 'routes.phpを通りました';
+// echo '<br />';
 
 // 1.GETパラメータを取得
 // explode関数：：第二引数の文字列を、第一引数の文字列で分解し、配列で返す関数。
@@ -9,7 +9,7 @@ $params = explode('/', $_GET['url']);
 
 // var_dump($params);
 //2.パラメータの分解（リソース名、アクション名、オプションを取得）
-$resouce = $params[0];
+$resource = $params[0];
 $action = $params[1];
 $id = 0;
 if (isset($params[2])){
@@ -17,6 +17,6 @@ if (isset($params[2])){
 }
 
 //3.コントローラの呼び出し
-require('controllers/'. $resouce .'_controller.php');
+require('controllers/'. $resource .'_controller.php');
 
 ?>
